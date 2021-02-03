a36581c69b91a437355065a5a6ab5ff48ddf9b11
Jaela-C <jaelacarlosama3c@hotmail.com>
2021-02-03 12:35:00 -0500

    Pagination

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import {Layout,Col, Card, Modal, Typography, Button, Pagination} from 'antd';
 import 'antd/dist/antd.css';
 
-
 const { Header } = Layout;
 const TextTitle = Typography.Title;
 
